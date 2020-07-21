d6f88c455a3eea09297f3338d9ea940336bcbaff
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-21 13:00:04 +0100

    make question one first fragment on main activity load

--- app/src/main/java/com/example/androidkotlinquizapp/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinquizapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidkotlinquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var questionOneInflatedInstance=QuizOneFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flQuiz,questionOneInflatedInstance)
+        }
+
     }
 }
