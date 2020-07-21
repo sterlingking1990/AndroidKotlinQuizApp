309369cd1cdf9ddd6aac8491b6bc289ad9964bcb
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-21 13:23:31 +0100

    replace fragments on click of button

--- app/src/main/java/com/example/androidkotlinquizapp/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinquizapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidkotlinquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentFactory
+import kotlinx.android.synthetic.main.frame_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,23 @@ class MainActivity : AppCompatActivity() {
 
         var questionOneInflatedInstance=QuizOneFragment()
 
+        var questionTwoInflatedInstance = QuizTwoFragment()
+
+
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.flQuiz,questionOneInflatedInstance)
+        }
+
+        btnQuizOne.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flQuiz,questionOneInflatedInstance)
+            }
+        }
+
+        btnQuizTwo.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flQuiz,questionTwoInflatedInstance)
+            }
         }
 
     }
